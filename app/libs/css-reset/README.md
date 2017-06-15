15aa058bc26cba22bcdd8b29ddd222c7e106347d
serbernar <serbernar1@gmail.com>
2017-06-15 07:56:44 +0300

    After initial commit, save skeleton for usage in next

---
+++ app/libs/css-reset/README.md
@@ -0,0 +1,25 @@
+CSS-Reset
+=========
+
+Improvements of [Eric Meyer's Reset CSS (v2.0)](http://meyerweb.com/eric/tools/css/reset/) :
+
+Installation
+------------
+
+### Bower
+
+`bower install css-reset`
+
+Releases Notes
+--------------
+
+* Version 2.1 : + `textarea { resize: none; }` to avoid textarea resizing. (08/10/2013)⋅⋅
+* Version 2.2 : + `a { outline: none; }` to avoid dots around links. (02/12/2013)⋅⋅
+* Version 2.3 : I implemented many stuff from [YUI 3.5.0 reset.css](http://yuilibrary.com/yui/docs/cssreset/). (03/12/2013)⋅⋅
+* Version 2.4 : + `body { -webkit-font-smoothing: antialiased; }` to avoid aliasing on Chrome. (21/01/2014)⋅⋅
+* Version 2.5 : + a `{ color: inherit; text-decoration: none; }` to avoid links auto-formatting. (28/01/2014)⋅⋅
+
+Package Manager
+---------------
+
+* [Composer](http://packagist.org/packages/components/css-reset): `components/jquery`
---
+++ gulpfile.js
@@ -0,0 +1,122 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    scss = require('gulp-scss'),
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache'),
+    autoprefixer = require('gulp-autoprefixer'),
+    ftp = require('vinyl-ftp'),
+    notify = require("gulp-notify"),
+    jade = require('gulp-jade');
+
+// Скрипты проекта
+
+gulp.task('common-js', function () {
+    return gulp.src([
+        'app/js/common.js'
+    ])
+        .pipe(concat('common.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('js', ['common-js'], function () {
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/jquery-eu-cookie-law-popup.js'
+    ])
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify()) // Минимизировать весь js (на выбор)
+        .pipe(gulp.dest('app/js'))
+        .pipe(gulp.dest('../static/js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false,
+        open: false
+    });
+});
+gulp.task('scss', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer(['last 15 versions']))
+        .pipe(cleanCSS()) // Опционально, закомментировать при отладке
+        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest('../static/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('templates', function () {
+    var YOUR_LOCALS = {};
+
+    gulp.src('app/jade/*.jade')
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest('app'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+
+gulp.task('watch', ['templates', 'scss', 'js', 'browser-sync'], function () {
+    // gulp.watch('app/sass/**/*.sass', ['sass']);
+    gulp.watch('app/jade/**/*.jade', ['templates']);
+    gulp.watch('app/scss/**/*.scss', ['scss']);
+    gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+    gulp.watch('app/index.html', browserSync.reload);
+});
+
+gulp.task('imagemin', function () {
+    return gulp.src('app/img/**/**/**/*')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('../static/img'));
+});
+
+gulp.task('build', ['removedist', 'imagemin', 'scss', 'js'], function () {
+
+    var buildFiles = gulp.src([
+        'app/templates/*.html',
+        'app/.htaccess'
+    ]).pipe(gulp.dest('../build_html'));
+
+    var buildCss = gulp.src([
+        'app/css/main.min.css'
+    ]).pipe(gulp.dest('../static/css'));
+
+    var buildJs = gulp.src([
+        'app/js/scripts.min.js'
+    ]).pipe(gulp.dest('../static/js'));
+
+    var buildFonts = gulp.src([
+        'app/fonts/**/*'
+    ]).pipe(gulp.dest('../static/fonts'));
+
+    var buildImg = gulp.src([
+        'app/img/**/**/**/*'
+    ]).pipe(gulp.dest('../static/img'));
+
+});
+
+
+gulp.task('removedist', function () {
+    return del.sync('dist');
+});
+gulp.task('clearcache', function () {
+    return cache.clearAll();
+});
+
+
+gulp.task('default', ['watch']);
+
