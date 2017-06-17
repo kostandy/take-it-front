507fd238135fb814dfd08568467fad7814144a0d
serbernar <serbernar1@gmail.com>
2017-06-17 21:55:22 +0300

    Updated structure, join and configure to deploy for backend Ended 2 of 5 parts

--- gulpfile.js
+++ gulpfile.js
@@ -16,25 +16,17 @@ var gulp = require('gulp'),
     jade = require('gulp-jade');
 
 // Скрипты проекта
-
-gulp.task('common-js', function () {
-    return gulp.src([
-        'app/js/common.js'
-    ])
-        .pipe(concat('common.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/js'));
-});
-
-gulp.task('js', ['common-js'], function () {
+gulp.task('js', function () {
     return gulp.src([
         'app/libs/jquery/dist/jquery.min.js',
-        'app/libs/jquery-eu-cookie-law-popup.js'
+        'app/libs/jquery-eu-cookie-law-popup.js',
+        'app/js/*.js',
+        '!app/js/scripts.min.js'
     ])
         .pipe(concat('scripts.min.js'))
-        .pipe(uglify()) // Минимизировать весь js (на выбор)
+        .pipe(uglify())
         .pipe(gulp.dest('app/js'))
-        .pipe(gulp.dest('../static/js'))
+        .pipe(gulp.dest('../backend/django_static/js'))
         .pipe(browserSync.reload({ stream: true }));
 });
 
@@ -54,14 +46,14 @@ gulp.task('scss', function () {
         .pipe(autoprefixer(['last 15 versions']))
         .pipe(cleanCSS()) // Опционально, закомментировать при отладке
         .pipe(gulp.dest('app/css'))
-        .pipe(gulp.dest('../static/css'))
+        .pipe(gulp.dest('../backend/django_static/css'))
         .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('templates', function () {
     var YOUR_LOCALS = {};
 
-    gulp.src(['app/jade/*.jade', '!app/jade/_*.jade'])
+    gulp.src(['app/jade/*.jade', '!app/jade/blocks', '!app/jade/pages'])
         .pipe(jade({
             locals: YOUR_LOCALS
         }))
@@ -74,38 +66,56 @@ gulp.task('watch', ['templates', 'scss', 'js', 'browser-sync'], function () {
     // gulp.watch('app/sass/**/*.sass', ['sass']);
     gulp.watch('app/jade/**/*.jade', ['templates']);
     gulp.watch('app/scss/**/*.scss', ['scss']);
-    gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
-    gulp.watch('app/index.html', browserSync.reload);
+    gulp.watch(['libs/**/*.js', 'app/js/custom.js'], ['js']);
 });
 
 gulp.task('imagemin', function () {
     return gulp.src('app/img/**/**/**/*')
         .pipe(cache(imagemin()))
-        .pipe(gulp.dest('../static/img'));
+        .pipe(gulp.dest('../backend/django_static/img'));
 });
 
-gulp.task('build', ['removedist', 'imagemin', 'scss', 'js'], function () {
+gulp.task('build-html', function () {
+    var YOUR_LOCALS = {};
+
+    gulp.src([
+        'app/*.html'
+    ]).pipe(gulp.dest('../backend/templates/src'));
+});
+
+
+/* Building html blocks for using in django temlates, KISS-DRY */
+gulp.task('bb-html', function () {
+    var YOUR_LOCALS = {};
+    gulp.src(['app/jade/blocks/*.jade'])
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest('../backend/templates/src/blocks'))
+});
+
+gulp.task('build', ['build-html','imagemin', 'scss', 'js'], function () {
 
     var buildFiles = gulp.src([
-        'app/templates/*.html',
-        'app/.htaccess'
-    ]).pipe(gulp.dest('../build_html'));
+        'app/*.html'
+    ])
+    .pipe(gulp.dest('../backend/templates/src'));
 
     var buildCss = gulp.src([
         'app/css/main.min.css'
-    ]).pipe(gulp.dest('../static/css'));
+    ]).pipe(gulp.dest('../backend/django_static/css'));
 
     var buildJs = gulp.src([
         'app/js/scripts.min.js'
-    ]).pipe(gulp.dest('../static/js'));
+    ]).pipe(gulp.dest('../backend/django_static/js'));
 
     var buildFonts = gulp.src([
         'app/fonts/**/*'
-    ]).pipe(gulp.dest('../static/fonts'));
+    ]).pipe(gulp.dest('../backend/django_static/fonts'));
 
     var buildImg = gulp.src([
         'app/img/**/**/**/*'
-    ]).pipe(gulp.dest('../static/img'));
+    ]).pipe(gulp.dest('../backend/django_static/img'));
 
 });
 
